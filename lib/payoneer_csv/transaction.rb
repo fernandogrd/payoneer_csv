c9b06215f96df0d18029d6f66931387b7caa6854
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-10-27 13:34:03 +0200

    Remove redundant methods.

--- lib/payoneer_csv/transaction.rb
+++ lib/payoneer_csv/transaction.rb
@@ -10,14 +10,6 @@ module PayoneerCsv
       @description = attributes[:description].strip
       @amount = attributes[:amount].to_f
     end
-
-    def direction
-      amount >=0 ? :deposit : :withdrawal
-    end
-
-    def created_on
-      created_at.split(/\s/).first
-    end
   end
 
 end
--- spec/payoneer_csv/transaction_spec.rb
+++ spec/payoneer_csv/transaction_spec.rb
@@ -16,32 +16,4 @@ describe PayoneerCsv::Transaction do
   its(:amount) { should == attributes[:amount].to_f }
   its(:created_at) { should == attributes[:created_at] }
 
-  describe '#direction' do
-    it { should respond_to(:direction) }
-
-    context 'when an amount is positive' do
-      before { attributes[:amount] = 123 }
-
-      it 'should return :deposit' do
-        expect(transaction.direction).to eq(:deposit)
-      end
-    end
-
-    context 'when an amount is negative' do
-      before { attributes[:amount] = -123 }
-
-      it 'should return :withdrawal' do
-        expect(transaction.direction).to eq(:withdrawal)
-      end
-    end
-  end
-
-  describe '#created_on' do
-    it { should respond_to(:direction) }
-
-    it 'should return only date' do
-      expect(transaction.created_on).to eq('10/19/2012')
-    end
-  end
-
 end
